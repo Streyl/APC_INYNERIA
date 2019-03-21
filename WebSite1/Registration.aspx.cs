35c5408e5215d729873004cd20b2226ac768af05
STREYL <assa5424@gmail.com>
2019-03-21 18:25:54 +0100

    working if

--- WebSite1/Registration.aspx.cs
+++ WebSite1/Registration.aspx.cs
@@ -19,37 +19,43 @@ public partial class Registration : System.Web.UI.Page
         }
         else
         {
-            Clear();
+            //Clear();
         }
     }
     private void AddNewUser()
     {
         using (LinqDataClassesDataContext objDataContext = new LinqDataClassesDataContext())
         {
-            user objUser = new user();
-            // fields to be insert
-            //objUser.id=+0;
-            objUser.name = tbName.Text;
-            objUser.surname = tbSurname.Text;
-            objUser.login = tbLogin.Text;
-            objUser.password = tbPassword.Text;
-            objUser.accountStatus = 1;
-            objUser.level = 1;
-            objDataContext.users.InsertOnSubmit(objUser);
-            // executes the commands to implement the changes to the database
-            objDataContext.SubmitChanges();
+            //foreach ()
+           // {
+                user objUser = new user();
+                // fields to be insert
+                //objUser.id=+0;
+                objUser.name = tbName.Text;
+                objUser.surname = tbSurname.Text;
+                objUser.login = tbLogin.Text;
+                objUser.password = tbPassword.Text;
+                objUser.accountStatus = 1;
+                objUser.level = 1;
+                objDataContext.users.InsertOnSubmit(objUser);
+                // executes the commands to implement the changes to the database
+                objDataContext.SubmitChanges();
+           // }
         };
     }
 
     protected void btSubmit_Click(object sender, EventArgs e)
     {
-        if (tbLogin.Text == "" || tbPassword.Text == "")
+        if(tbLogin.Text == "" || tbPassword.Text == "")
         {
             lbErrorMessage.Text = "U have to fill Login and Password columns";
+            Clear();
+            
         }
         else if (tbPassword.Text != tbConfirmPassword.Text)
         {
-            lbErrorMessage.Text = "Passwords do not match";
+            lbErrorMessage.Text = "Passwords does not match";
+            Clear();
         }
         else
         {
@@ -61,8 +67,6 @@ public partial class Registration : System.Web.UI.Page
     void Clear()
     {
         tbName.Text = tbSurname.Text = tbLogin.Text = tbPassword.Text = tbConfirmPassword.Text = "";
-        hfid.Value = "";
-        lbErrorMessage.Text = "";
     }
 }
 //Problems:
