bd0e66dce779cba20595188e0be5ace91c11f813
STREYL <oleksandr.kazanzhy@gmail.com>
2019-06-03 19:12:59 +0200

    Stats

---
+++ WebSite1/Stats.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Title="Stats" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Stats.aspx.cs" Inherits="Stats" %>
+
+	<asp:Content ID="IDPageBody" ContentPlaceHolderID="MainContent" runat="server">
+        Without Graphs
+        <table>
+            <tr>
+                <td cosplan="2">
+                    Project Stats 
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+                     <asp:Panel ID="PanelPDF" runat="server">
+                                    <asp:GridView ID="GridViewData" runat="server" AutoGenerateColumns="False">
+                                        <%--<Columns>
+                                            <asp:BoundField HeaderText="Name" DataField="name" />
+                                            <asp:BoundField HeaderText="Creator" DataField="Name" />
+                                        </Columns>--%>
+
+                                    </asp:GridView>
+
+                                </asp:Panel>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                        <asp:Button ID="btDownload"  runat="server" Text="Download" OnClick="btDownload_Click" />
+                </td>
+            </tr>
+           
+            
+</table>
+        <hr />
+<table>
+    <tr>
+        <td cosplan="2">
+            Task Stats
+
+        </td>
+    </tr>
+    <tr>
+                <td>
+                     <asp:Panel ID="PanelPDF2" runat="server">
+                                    <asp:GridView ID="GridViewTasksData" runat="server">
+
+
+                                    </asp:GridView>
+
+                                </asp:Panel>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                        <asp:Button ID="btDownloadTasks"  runat="server" Text="Download" OnClick="btDownloadTasks_Click" />
+                </td>
+            </tr>
+</table>
+
+ 	</asp:Content>
+	<asp:Content ID="Content1" ContentPlaceHolderID="NotLogInContent" runat="server">
+
+				U have to Log IN
+
+ 	</asp:Content>
---
+++ WebSite1/Stats.aspx.cs
@@ -0,0 +1,88 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+public partial class Stats : System.Web.UI.Page
+{
+    LinqDataClassesDataContext BaseDB = new LinqDataClassesDataContext();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var Data = (from ob in BaseDB.projects
+                    select ob);
+        GridViewData.DataSource = Data;
+        GridViewData.DataBind();
+
+        var Data1 = (from ob in BaseDB.tasks
+                     select ob);
+        GridViewTasksData.DataSource = Data1;
+        GridViewTasksData.DataBind();
+
+
+    }
+
+    protected void btDownload_Click(object sender, EventArgs e)
+    {
+        Response.ContentType = "Stats/pdf";
+        Response.AddHeader("content-disposition", "attachment;filename=print.pdf");
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+
+        StringWriter sw = new StringWriter();
+        HtmlTextWriter hw = new HtmlTextWriter(sw);
+
+        PanelPDF.RenderControl(hw);
+
+        StringReader sr = new StringReader(sw.ToString());
+        Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 10f);
+        HTMLWorker htmlParser = new HTMLWorker(pdfDoc);
+        PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+
+        pdfDoc.Open();
+        htmlParser.Parse(sr);
+        pdfDoc.Close();
+
+        Response.Write(pdfDoc);
+        Response.End();
+
+
+
+    }
+    public override void VerifyRenderingInServerForm(Control control)
+    {
+        return;
+    }
+
+
+
+    protected void btDownloadTasks_Click(object sender, EventArgs e)
+    {
+        Response.ContentType = "Task_Stats/pdf";
+        Response.AddHeader("content-disposition", "attachment;filename=print.pdf");
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+
+        StringWriter sw1 = new StringWriter();
+        HtmlTextWriter hw1 = new HtmlTextWriter(sw1);
+
+        PanelPDF2.RenderControl(hw1);
+
+        StringReader sr1 = new StringReader(sw1.ToString());
+        Document pdfDoc1 = new Document(PageSize.A4, 10f, 10f, 100f, 10f);
+        HTMLWorker htmlParser = new HTMLWorker(pdfDoc1);
+        PdfWriter.GetInstance(pdfDoc1, Response.OutputStream);
+
+        pdfDoc1.Open();
+        htmlParser.Parse(sr1);
+        pdfDoc1.Close();
+
+        Response.Write(pdfDoc1);
+        Response.End();
+    }
+}
